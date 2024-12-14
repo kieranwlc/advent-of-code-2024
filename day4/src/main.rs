a61d8283bfe2d72638c338d8481afcbfafcf2542
kieran <kierancheung@live.co.uk>
2024-12-14 22:52:22 +0000

    d4p2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -9,7 +9,7 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn check_character(
+fn check_xmas(
     text: &Vec<Vec<char>>, 
     x: usize, y: usize,
     xdir: isize, ydir: isize,
@@ -33,11 +33,11 @@ fn check_character(
     if nexty < 0 || nexty >= text.len() as isize { return };
 
     if text[nexty as usize][nextx as usize] == target {
-        check_character(text, nextx as usize, nexty as usize, xdir, ydir, matches);
+        check_xmas(text, nextx as usize, nexty as usize, xdir, ydir, matches);
     }
 }
 
-fn check_start(
+fn check_xmas_start(
     text: &Vec<Vec<char>>, 
     x: usize, y: usize,
     matches: &mut u32
@@ -45,8 +45,45 @@ fn check_start(
     for cy in -1..=1 {
         for cx in -1..=1 {
             if cy == 0 && cx == 0 { continue; }
-            check_character(text, x, y, cx, cy, matches);
+            check_xmas(text, x, y, cx, cy, matches);
         }
+    }
+}
+
+fn check_crossmas(
+    text: &Vec<Vec<char>>, 
+    x: usize, y: usize,
+    matches: &mut u32
+) {
+    if x == 0 || x == text[y].len() - 1 { return; }
+    if y == 0 || y == text.len() - 1 { return; }
+
+    let topleft = match text[y-1][x-1] {
+        'M' => 'M',
+        'S' => 'S',
+        _ => {return;}
+    };
+
+    let topright = match text[y-1][x+1] {
+        'M' => 'M',
+        'S' => 'S',
+        _ => {return;}
+    };
+
+    let botleft = match text[y+1][x-1] {
+        'M' => 'M',
+        'S' => 'S',
+        _ => {return;}
+    };
+
+    let botright = match text[y+1][x+1] {
+        'M' => 'M',
+        'S' => 'S',
+        _ => {return;}
+    };
+
+    if (topleft != botright) && (botleft != topright) {
+        *matches += 1;
     }
 }
 
@@ -67,14 +104,20 @@ fn main() {
     }
 
     let mut matches: u32 = 0;
+    let mut cross_matches: u32 = 0;
 
     for y in 0..chars.len() {
         for x in 0..chars[y].len() {
             if chars[y][x] == 'X' {
-                check_start(&chars, x, y, &mut matches);
+                check_xmas_start(&chars, x, y, &mut matches);
+            }
+
+            if chars[y][x] == 'A' {
+                check_crossmas(&chars, x, y, &mut cross_matches);
             }
         }
     }
 
     println!("Matches: {}", matches);
+    println!("Crossmass Matches: {}", cross_matches);
 }
